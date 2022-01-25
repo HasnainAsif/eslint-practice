7a35ea6372e80c1d9e0b4a861ad54ca96b253321
Hasnain Asif <hasnain.asif@invozone.com>
2022-01-25 14:49:08 +0500

    eslint practice with js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const restify = require('restify');
 
-const a = 5;
+// const a = 5;
+// console.log(a);
 
 const PORT = 5000;
 const app = restify.createServer();
@@ -10,4 +11,16 @@ const asd = () => {};
 asd();
 
 app.get('/', (req, res) => res.send({ hello: 'World' }));
+
+app.get('/test', (req, res) => {
+  const a = req.url;
+  return res.send({ msg: a });
+});
+
+const obj = {
+  hello() {
+    console.log('hello');
+  },
+};
+
 app.listen(PORT);
